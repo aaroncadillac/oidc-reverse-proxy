c26742d05512e8e094a19d3d155673f0c29af272
Aarón Hinojosa <aaron.hinojosa@quadsci.ai>
2025-06-11 03:07:15 -0600

    fix: add env vars eval support [patch]

--- utils/config-file.js
+++ utils/config-file.js
@@ -1,10 +1,26 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
+import fs from 'fs';
 
+const evalEnvVars = (data) => {
+  try {
+    const varsRegex = /\${([A-Za-z_][A-Za-z0-9_]*)}|\$([A-Za-z_][A-Za-z0-9_]*)/g;
+    for (const match of data.matchAll(varsRegex)) {
+      const varEval = process.env[match[1] || match[2]];
+      if (varEval) {
+        data = data.replace(match[0], varEval);
+      } else {
+        console.error(`Missing env var ${match[0]} on config file`);
+      }
+    }
+    return data;
+  } catch (error) {
+    console.error('Error replacing env vars', error);
+  }
+}
 const readConfigFile = (path) => {
   try {
-    const config = require(path);
-    return config;
+    let data = fs.readFileSync(path, 'utf8');
+    data = evalEnvVars(data);
+    return JSON.parse(data);
   } catch (error) {
     console.error('Error reading config file', error);
   }
